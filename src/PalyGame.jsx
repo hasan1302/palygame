68b471b409cc12ab0561193e620288db89da0baf
Timur <hasan13@me.com>
2017-12-08 01:52:29 +0300

    последняя
    посл

--- src/PalyGame.jsx
+++ src/PalyGame.jsx
@@ -13,7 +13,6 @@ class PalyGame extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
         playerOne: this.props.playerOne, 
         playerOneWords: [],
         playerOneScore: 0, 
@@ -26,14 +25,6 @@ class PalyGame extends Component {
         word: "",
         words: [""],
         timer: 30
-=======
-        playerOne: localStorage.getItem("playerOneName"),
-        playerOneScore: localStorage.getItem("playerScoreOne"),
-        playerTwo: localStorage.getItem("playerTwoName"),
-        playerTwoScore: localStorage.getItem("playerScoreTwo"),
-        playerTurn: localStorage.getItem("playerTurn"),
-        word: ""
->>>>>>> origin/master
     }
   };
 
@@ -50,7 +41,6 @@ class PalyGame extends Component {
   }
   
   playerScored = () => {
-<<<<<<< HEAD
     let word = this.state.word;
     this.state.words.push(word);
     if (this.state.playerTurn === this.state.playerOne) {
@@ -59,23 +49,10 @@ class PalyGame extends Component {
     } else {
         this.state.playerTwoWords.push(word)
         this.setState({playerTwoScore: ++this.state.playerTwoScore})
-=======
-    if  (this.state.playerTurn === (null || "1") ) {
-        let score = localStorage.getItem("playerScoreOne") ? 1: 0;
-        score++;
-        localStorage.setItem("playerTurn", "2");
-        localStorage.setItem("playerScoreOne", score)
-    } else {
-        let score = (!localStorage.getItem("playerScoreTwo") ? 1: 0);
-        score++;
-        localStorage.setItem("playerTurn", "1")
-        localStorage.setItem("playerScoreTwo", score);
->>>>>>> origin/master
     }
     this.setState({timer: 30});
   }
 
-<<<<<<< HEAD
   submitWord = (event) => {
     if (palindrome(this.state.word) === true) {
         if (this.state.words.includes(this.state.word)) {
@@ -124,44 +101,13 @@ class PalyGame extends Component {
                     )}
                 </div>);
     }
-=======
-  renderPlayerOne = () => {
-      return (
-                <div>
-                    <h1> {this.state.playerOne} </h1>
-                </div>
-            )
-  }
-
-
-
-  submitWord = () => {
-    if (this.state.word === "volodimirtrunin") {
-        alert("GAME OVER");
-        this.setState({winner: localStorage.getItem("playerTurn")})
-    }
-     if ((palindrome(this.state.word) == true)) {    ///USE REGEXP PALYNDROME
-        $.post(postWordUrl, {word: this.state.word} );
-
-        ///////////////*^^((((((((((((^(794947974979794779447794479974947479479479479794479)))))))))))))
-        // if inMongoBase then {v   <-------
-        this.playerScored();
-        // }
-/////////////////////////7447144582895599353469004707070047407470470704704470470470            TUT OSTAMOVILSA             !!!!!!!!!!!!!
-     } else {alert("Your word is not a palyndrome!")}
-  }
-
-  typingWord = (event) => {
-      this.setState({word: event.target.value});
->>>>>>> origin/master
   }
 
 
 
   render() {
-    return (
+    return ( 
         <div>
-<<<<<<< HEAD
             {this.state.currentCount}
             {this.showWordsPlayerOne()}
             {this.showWordsPlayerTwo()}
@@ -179,15 +125,6 @@ class PalyGame extends Component {
         
         
         
-=======
-        <h1>PalyGame {this.state.playerOne}: {this.state.playerOneScore} versus {this.state.playerTwo}: {this.state.playerTwoScore}</h1>
-        <form onSubmit={this.submitWord} >
-                    <input onChange={this.typingWord} type="text" placeholder="Any Palyndrome Word" name="palyndrome text" required></input>
-                    <input type="submit" value="SEND" />
-                </form>
-        </div>
-
->>>>>>> origin/master
     );
 
   };
